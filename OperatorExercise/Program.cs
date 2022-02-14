137b323017e0d0c0e2eea92c061efc8e1d78834a
Maddi Wade <Madelinenwade@gmail.com>
2022-02-14 13:02:56 -0500

    finished exercise 2

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -6,18 +6,33 @@ namespace OperatorExercise
     {
         static void Main(string[] args)
         {
+            
             int a = 17;
             int b = 4;
             int quotient = a / b;
             int remainder = a % b;
-          
-            if (a == 17 && b == 4)
+            AreaOfCircle();
+            
+
+
+
+
+            if (a == 17 && b == 4)            
             {
+               //Exercise one
                 Console.WriteLine($"{a}/{b} is {quotient} remainder  {remainder}");
             }
-           
+
         }
    
+            static void AreaOfCircle()
+            {
+                Console.WriteLine("What is the radius of your circle");
+                var radius = double.Parse(Console.ReadLine());
+                double pi = 3.14;
+                var formula = pi * radius * radius;
+                Console.WriteLine($"The area of a circle with a radius of {radius} is {formula}");
+            }
         
          
         
