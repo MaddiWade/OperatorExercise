177baf2f980f4985dc558d87027110bc25e15328
Maddi Wade <Madelinenwade@gmail.com>
2022-02-14 11:54:33 -0500

    Add project files.

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
